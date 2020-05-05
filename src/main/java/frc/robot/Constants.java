592331004cc5caac5056bdbf2540fff89a77500a
vatanaksoytezer <vatanaksoytezer@gmail.com>
2020-05-05 22:52:51 +0300

    Add another commit in a new branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -49,6 +49,7 @@ public final class Constants {
         public static final int lEncoderPortA = 6;
         public static final int lEncoderPortB = 7;
         public static final int extraEncoder = 8;
+        // New additions
 
     }
 
